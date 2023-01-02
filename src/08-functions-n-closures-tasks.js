15b5a213be4691c92cff6c250454938074d83b62
Anton Vyun <zeta664433@gmail.com>
2023-01-03 00:20:08 +0300

    feat: solve 08-function-n-closures-tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,9 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  const fn = (x) => f(g(x));
+  return fn;
 }
 
 
@@ -44,8 +45,9 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const power = (x) => x ** exponent;
+  return power;
 }
 
 
@@ -81,8 +83,18 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  const cached = (n) => {
+    if (n in cache) {
+      return cache[n];
+    }
+    cache[n] = func(n);
+    return cache[n];
+  };
+
+  return cached;
 }
 
 
@@ -147,8 +159,15 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const array = args1;
+  const fn1 = (...x) => {
+    for (let i = 0; i < x.length; i += 1) {
+      array.push(x[i]);
+    }
+    return fn(...array);
+  };
+  return fn1;
 }
 
 
@@ -169,8 +188,15 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+
+  const counter = () => {
+    count += 1;
+    return count;
+  };
+
+  return counter;
 }
 
 
